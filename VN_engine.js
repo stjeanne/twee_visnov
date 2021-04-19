caa38fb36e52f059669dc7c259d856435d917c7a
jeanne <jwthornton@gmail.com>
2021-04-19 13:11:57 -0400

    added canvas and roughish layout

--- VN_engine.js
+++ VN_engine.js
@@ -1,5 +1,31 @@
 /* dialogue js function */
 
+let processScript = new function () {
+	/*
+		This is called at the start of the story. It crawls through the scripts and loads everything into JSON for easy parsing.
+		
+
+	*/
+}
+
 let parseScene = new function () {
 	
+	// it 
+
+}
+
+let makeCanvas = new function () {
+	/*
+		- generate div
+		
+
+
+	*/
+}
+
+let renderCanvas = new function () {
+	/*
+		- draw the background art
+		- 
+	*/
 }
